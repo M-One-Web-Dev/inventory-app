54967adec057a92fff2fdffd23279231e8a8235a
Nurrohmann <nurrohmanmarduki@gmail.com>
2024-07-29 08:59:54 +0700

    feat: dialog delete active student

--- resources/js/components/dashboard/activeStudent/dialog/index.js
+++ resources/js/components/dashboard/activeStudent/dialog/index.js
@@ -3,3 +3,4 @@ export * from "./DialogEditTeacher";
 export * from "./DialogAddTeacher";
 export * from "./DialogImportExcel";
 export * from "./DialogDeleteActiveStudent";
+export * from "./DialogAddActiveStudent";
