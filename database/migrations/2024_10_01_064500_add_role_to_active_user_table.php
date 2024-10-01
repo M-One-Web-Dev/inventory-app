7cc63ece454e758f12855a63a0ced3039513e8e6
Nuuriii <nuriii.dev@gmail.com>
2024-10-01 13:46:58 +0700

    feat: role in active user

--- database/migrations/2024_10_01_064500_add_role_to_active_user_table.php
+++ database/migrations/2024_10_01_064500_add_role_to_active_user_table.php
@@ -4,25 +4,29 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class AddRoleToActiveUserTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
     public function up(): void
     {
         Schema::table('active_user', function (Blueprint $table) {
-            //
+            $table->string('role')->nullable(); // Menambahkan kolom role
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
     public function down(): void
     {
         Schema::table('active_user', function (Blueprint $table) {
-            //
+            $table->dropColumn('role'); // Menghapus kolom role
         });
     }
-};
+}
