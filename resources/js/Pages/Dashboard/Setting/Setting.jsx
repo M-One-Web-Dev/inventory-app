b360884b01f6cf593e67c22343f1c90976c4f4e6
Nuuriii <nuriii.dev@gmail.com>
2024-11-25 08:41:31 +0700

    feat: create setting page ui

--- resources/js/Pages/Dashboard/Setting/Setting.jsx
+++ resources/js/Pages/Dashboard/Setting/Setting.jsx
@@ -5,7 +5,7 @@ import ComingSoon from "@/components/ui/comingSoon";
 function Setting() {
     return (
         <div className="relative w-full">
-            {/* <div className="absolute top-[-40px] w-full px-[20px]">
+            <div className="absolute top-[-40px] w-full px-[20px]">
                 <div className="bg-white shadow-[3px_3px_20px_-2px_#00000024]  py-[20px] rounded-md px-[20px]">
                     <h1 className="text-[20px]">Setting</h1>
                 </div>
@@ -17,39 +17,28 @@ function Setting() {
                     </h1>
 
                     <div className="flex flex-col mt-[50px] gap-5">
-                        <label className="flex justify-between" htmlFor="years">
-                            <span className="w-[40%] text-[13px]">
+                        <div className="flex flex-col gap-1">
+                            <p className="w-[40%] text-[13px]">
                                 Tahun Pelajaran
                                 <span className="text-red-500">*</span>
-                            </span>
+                            </p>
 
                             <input
                                 className="rounded-sm border-gray-300 py-[5px] border-[1px] border-solid flex-grow"
                                 type="text"
                             />
-                        </label>
-
-                        <label className="flex justify-between" htmlFor="years">
-                            <span className="w-[40%] text-[13px]">
-                                Tahun Pelajaran
-                                <span className="text-red-500">*</span>
-                            </span>
-
-                            <input
-                                className="rounded-sm border-gray-300 py-[5px] border-[1px] border-solid flex-grow"
-                                type="text"
-                            />
-                        </label>
+                            <span className="text-[11px]">EX: 2022/2023</span>
+                        </div>
 
                         <div className="w-full flex justify-end text-white mt-[30px]">
                             <button className="bg-violet-600 text-[13px] py-[7px] px-[13px] rounded-md font-semibold">
-                                Submit
+                                Update
                             </button>
                         </div>
                     </div>
                 </div>
-            </div> */}
-            <ComingSoon />
+            </div>
+            {/* <ComingSoon /> */}
         </div>
     );
 }
